5c11b545e9307ee61ddccd71eed66c26e8f4d9bf
Your Name <nickmihaly@gmail.com>
2021-05-30 22:14:04 -0700

    add fetch api function

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,31 @@
+var city = "";
+var currentTemperature = document.querySelector(".temp");
+var currentHumidty = document.querySelector(".humidity");
+var currentWSpeed = document.querySelector(".wind");
+var currentUvindex = document.querySelector(".uvIndex");
+var currentCity = document.querySelector(".cityName")
+var citySearch = document.querySelector(".searchInput")
+var searchCityBtn = document.querySelector(".searchBtn");
+
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=60f54ac03ed7a4b3ce28598e56d34aee";
+
 var getWeatherInfo = function() {
-  fetch("function was called");
-  };
+  fetch(apiURL)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data.main.temp);
+        
+    });
+  }
+});
+}
+
+searchCityBtn.addEventListener("click", getWeatherInfo);
+
+
+
   
-  getWeatherInfo();
+ 
